e7871b992587c37013b3dc09b54a1f725a0ff773
Noah <crashlive112@gmail.com>
2021-11-22 19:35:53 -0500

    commit tree 21886efc59852af4558c5cb05c5f90846577773f

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // const array = word.split('')
+  // const reverseArray = array.reverse()
+  // const joinReversedArray = reverseArray.join('')
+  const reversedWord = word.split('').reverse().join('')
+  return reversedWord === word ? true : false
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+  reverse word and store as variable
+    - .split word (is stored as array)
+    - .reverse() to reverse array
+    - .join() to put word back together (store as wordReverse)
+  check for equality with original word
+  if equal return true
+  else false
 */
 
 /*
